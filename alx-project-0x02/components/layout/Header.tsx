1272fc3a4e4e4aaa4af43260b8095e340828179c
Yared <yaredhabtamu08@gmail.com>
2025-07-25 14:00:45 +0300

    task 1 updated

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,1 +1,9 @@
-// Header component placeholder
+import React from "react";
+
+const Header: React.FC = () => (
+  <header>
+    <h1>ALX Project 0x02 Header</h1>
+  </header>
+);
+
+export default Header;
---
+++ alx-project-0x02/interfaces/index.ts
@@ -0,0 +1,6 @@
+// Place your shared TypeScript interfaces here
+
+export interface ExampleInterface {
+  id: number;
+  name: string;
+}
